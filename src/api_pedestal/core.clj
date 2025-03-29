c3bde1486adefeadd2d2d50c8a7c8ea12bcc6977
Eliel.calore.zougue <eliel.calore@zougue.com>
2025-03-29 18:50:38 -0300

    Gardening e criação de testes unitarios e arquivo de teste postaman

--- src/api_pedestal/core.clj
+++ src/api_pedestal/core.clj
@@ -1,6 +1,19 @@
-(ns api-pedestal.core)
-(prn "Started de app")
+(ns api-pedestal.core
+  (:require [com.stuartsierra.component :as component]
+            [api-pedestal.database :as database]
+            [api-pedestal.rotas :as rotas]
+            [api-pedestal.servidor :as servidor]))
 
+(def my-component-system
+  (component/system-map
+    :database (database/new-database)
+    :rotas (rotas/new-rotas)
+    :servidor (component/using (servidor/new-servidor) [:database :rotas])))
+
+(defn -main []
+  (println "Iniciando o sistema...")
+  (component/start my-component-system)
+  (println "Sistema iniciado!"))
 
 
 
--- src/api_pedestal/rotas.clj
+++ src/api_pedestal/rotas.clj
@@ -2,56 +2,68 @@
   (:require [io.pedestal.http.route :as route]
             [com.stuartsierra.component :as component]))
 
+;; ========= HANDLERS  =========
+
+(defn listar-tarefas [request]
+  {:status 200
+   :body   @(get request :store)})
+
+(defn criar-tarefa-mapa [uuid nome status]
+  {:id uuid :nome nome :status status})
+
+(defn criar-tarefa [request]
+  (prn "request: " request)
+  (prn "Body params:" (:body-params request))
+  (let [uuid (java.util.UUID/randomUUID)
+        nome (get-in request [:json-params  :nome])
+        status (get-in request [:json-params  :status])
+        tarefa (criar-tarefa-mapa uuid nome status)
+        store (get request :store)]
+    (swap! store assoc uuid tarefa)
+    {:status 200
+     :body {:mensagem "Tarefa registrada com sucesso!"
+            :tarefa   tarefa}}))
+
+(defn funcao-hello [request]
+  {:status 200
+   :body (str "Hello World " (get-in request [:json-params  :name]))})
+
+(defn remover-tarefa [request]
+  (let [store (get request :store)
+        tarefa-id (get-in request [:path-params :id])
+        tarefa-id-uuid (java.util.UUID/fromString tarefa-id)]
+    (swap! store dissoc tarefa-id-uuid)
+    {:status 200 :body {:mensagem "Removida com sucesso!"}}))
+
+(defn atualizar-tarefa [request]
+  (let [tarefa-id (get-in request [:path-params :id])
+        tarefa-id-uuid (try
+                         (java.util.UUID/fromString tarefa-id)
+                         (catch IllegalArgumentException e
+                           (throw (ex-info "ID inválido" {:id tarefa-id}))))
+        nome (get-in request [:query-params :nome])
+        status (get-in request [:query-params :status])
+        tarefa (criar-tarefa-mapa tarefa-id-uuid nome status)
+        store (get request :store)]
+    (swap! store assoc tarefa-id-uuid tarefa)
+    {:status 200
+     :body {:mensagem "Tarefa atualizada com sucesso!"
+            :tarefa   tarefa}}))
+                                               ;; Responde com a mensagem de sucesso e os dados da tarefa
+;; ========= COMPONENT =========
+
 (defrecord Rotas []
   component/Lifecycle
 
   (start [this]
     (prn "Start rotas")
-    (defn listar-tarefas [request]
-      {:status 200 :body @(:store request)})
-
-    (defn criar-tarefa-mapa [uuid nome status]
-      {:id uuid :nome nome :status status})
-
-    (defn criar-tarefa [request]
-      (let [uuid (java.util.UUID/randomUUID)
-            nome (get-in request [:query-params :nome])
-            status (get-in request [:query-params :status])
-            tarefa (criar-tarefa-mapa uuid nome status)
-            store (:store request)]
-        (swap! store assoc uuid tarefa)
-        {:status 200 :body {:mensagem "Tarefa registrada com sucesso!"
-                            :tarefa   tarefa}}))
-
-    (defn funcao-hello [request]
-      {:status 200 :body (str "Hello World " (get-in [:query-params :name] request))})
-
-    (defn remover-tarefa [request]
-      (let [store (:store request)
-            tarefa-id (get-in request [:path-params :id])
-            tarefa-id-uuid (java.util.UUID/fromString tarefa-id)]
-        (swap! store dissoc tarefa-id-uuid)
-        {:status 200 :body {:mensagem "Removida com sucesso!"}}))
-
-    (defn atualizar-tarefa [request]
-      (let [tarefa-id (get-in request [:path-params :id])
-            tarefa-id-uuid (java.util.UUID/fromString tarefa-id)
-            nome (get-in request [:query-params :nome])
-            status (get-in request [:query-params :status])
-            tarefa (criar-tarefa-mapa tarefa-id-uuid nome status)
-            store (:store request)]
-        (swap! store assoc tarefa-id-uuid tarefa)
-        {:status 200 :body {:mensagem "Tarefa atualizada com sucesso!"
-                            :tarefa   tarefa}}))
-
-    (def routes (route/expand-routes
-                  #{["/hello" :get funcao-hello :route-name :hello-world]
-                    ["/tarefa" :post criar-tarefa :route-name :criar-tarefa]
-                    ["/tarefa" :get listar-tarefas :route-name :listar-tarefas]
-                    ["/tarefa/:id" :delete remover-tarefa :route-name :remover-tarefa]
-                    ["/tarefa/:id" :patch atualizar-tarefa :route-name :atualizar-tarefa]}))
-
-    (assoc this :endpoints routes))
+    (let [routes (route/expand-routes
+                   #{["/hello" :get funcao-hello :route-name :hello-world]
+                     ["/tarefa" :post criar-tarefa :route-name :criar-tarefa]
+                     ["/tarefa" :get listar-tarefas :route-name :listar-tarefas]
+                     ["/tarefa/:id" :delete remover-tarefa :route-name :remover-tarefa]
+                     ["/tarefa/:id" :patch atualizar-tarefa :route-name :atualizar-tarefa]})]
+      (assoc this :endpoints routes)))
 
   (stop [this]
     (prn "Stop rotas")
@@ -59,12 +71,3 @@
 
 (defn new-rotas []
   (->Rotas))
-
-
-
-
-
-
-
-
-
